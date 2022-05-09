1fcbc4b179c8453d33aa9417807670106db09e06
RidwanMuhammad19 <ridwanm343@gmail.com>
2022-05-09 17:17:44 +0700

    fix : education

--- src/pages/api/education.ts
+++ src/pages/api/education.ts
@@ -3,28 +3,28 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default (req: NextApiRequest, res: NextApiResponse) => {
 	const education = [
 		{
-			institute: 'Waktu Kita',
-			website: 'https://www.waktukita.com/',
-			course: 'React Js',
-			endDate: '2021-06-20',
+			institute: 'Bootcamp React Js',
+			website: 'https://refactory.id/',
+			course: 'Refactory',
+			endDate: '2021-02-6',
 			place: 'boarding school',
-			technologies: [
-				'HTML',
-				'CSS',
-				'Javascript',
-				'php',
-				'Laravel',
-				'Algorithms',
-				'Data Structures',
-			],
+			technologies: ['Javascript', 'React Js'],
 		},
 		{
-			institute: 'Darussalam Gontor, Modern Islamic Institution',
-			website: 'https://gontor.ac.id/',
-			course: 'Islmic Teacher Training College',
-			endDate: '2018-06-30',
+			institute: 'Fundamental React Js',
+			website: 'https://www.waktukita.com/',
+			course: 'Waktu Kita',
+			endDate: '2020-09-09',
 			place: 'boarding school',
-			technologies: ['Life teaching'],
+			technologies: ['HTML', 'CSS', 'Javascript', 'React Js'],
+		},
+		{
+			institute: 'Bacelor Information System',
+			website: 'https://home.amikom.ac.id/',
+			course: 'Universitas Amikom Yogyakarta',
+			endDate: '2019-11-10',
+			place: 'Yogyakarta',
+			technologies: ['VB.net', 'fundamental Website'],
 		},
 	];
 
